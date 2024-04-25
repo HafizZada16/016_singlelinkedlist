2eca99d273d6ceceea373656f5ed5812a4a33cde
HafizZada16 <hafizzada16@gmail.com>
2024-04-25 17:10:57 +0700

    Add While If untuk mencari node yg punya nilai nim yg sama dgn nim yg dicari

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -32,5 +32,19 @@ void addnode() {
         return;
     }
 
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
     
 }
